8f77e09204b8b856ff5b02b126e4a339cce2ee6e
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-02-07 12:02:05 -0500

    chore: update flake inputs and add justfile
    - Update nixpkgs, zig2nix, and flake-utils to latest versions
    - Add explicit nixpkgs and nixpkgs-stable inputs to flake.nix
    - Add justfile with build-nix recipe

--- flake.nix
+++ flake.nix
@@ -2,11 +2,14 @@
   description = "zmx - session persistence for terminal processes";
 
   inputs = {
+    utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/fa83fd837f3098e3e678e6cf017b2b36102c7211";
+    nixpkgs.url = "github:NixOS/nixpkgs/54b154f971b71d260378b284789df6b272b49634";
     zig2nix.url = "github:Cloudef/zig2nix";
   };
 
   outputs =
-    { zig2nix, ... }:
+    { zig2nix, nixpkgs, nixpkgs-stable, utils, ... }:
     let
       flake-utils = zig2nix.inputs.flake-utils;
     in
